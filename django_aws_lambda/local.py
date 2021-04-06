696cf6d56ba4963ac1e6aaee6d7206cadf2ab318
Vadym Khodak <vadym.khodak@datarobot.com>
2021-04-06 08:54:32 +0300

    update settings and configurations

---
+++ django_aws_lambda/local.py
@@ -0,0 +1,14 @@
+from .settings import *  # noqa
+
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ROOT_DIR / "db.sqlite3",
+    }
+}
