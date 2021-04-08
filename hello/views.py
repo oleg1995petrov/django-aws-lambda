2d5512cd85cdf91b7815b14167de8d9b200f989f
Vadym Khodak <vadym.khodak@datarobot.com>
2021-04-08 10:22:35 +0300

    update view for hello app

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def hello(request, resource=None):
 
-    html = f"<h1>Hello {resource or 'World'}</h1>"
-    return HttpResponse(html)
+    return render(request, "index.html", {"name": resource or 'World'})
